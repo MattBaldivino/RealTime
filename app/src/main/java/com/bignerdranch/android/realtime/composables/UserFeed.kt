a16f6aa61a7901a7ff5121626d502a9420556e38
Cat <catdavis25@gmail.com>
2024-12-09 01:52:24 -0800

    user can scroll now need to fix styling

--- app/src/main/java/com/bignerdranch/android/realtime/composables/UserFeed.kt
+++ app/src/main/java/com/bignerdranch/android/realtime/composables/UserFeed.kt
@@ -1,6 +1,7 @@
 import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
@@ -8,6 +9,10 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -20,42 +25,13 @@ import database.AppDatabase
 import java.util.Date
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.bignerdranch.android.realtime.Posts
 
 
 @Composable
-fun UserFeed (navController: NavHostController, ) {
-    val context = LocalContext.current
-    val postsLists = getUserInfo(context)
-    for (i in postsLists.indices) {
-        println(postsLists[i])
-    }
-    println(postsLists.joinToString(", "))
-
-    Box(modifier = Modifier.fillMaxSize().background(Color.White)) {
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .align(Alignment.Center),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
-        )
-        {
-            for (i in 0 until postsLists.size) {
-                postsLists[i].owner?.let { Text(text = it) }
-                val currentImage = postsLists[i].image?.let { convertImageByteArrayToBitmap(it) }
-                currentImage?.let { BitmapImage(it) }
-            }
-            Button(
-                onClick = {
-                    navController.navigate("home") // Navigate to the Camera Screen
-                },
-                modifier = Modifier.padding(bottom = 40.dp) // Add padding to position below the first button
-            ) {
-                Text(text = "Home")
-            }
-        }
-    }
+fun UserFeed (navController: NavHostController) {
+    LazyCol(navController)
 }
 
 fun getUserInfo(context: Context): List<Posts> {
@@ -76,4 +52,32 @@ fun BitmapImage(bitmap: Bitmap) {
 
 fun convertImageByteArrayToBitmap(imageData: ByteArray): Bitmap {
     return BitmapFactory.decodeByteArray(imageData, 0, imageData.size)
+}
+
+@Composable
+fun UserPost(owner: String){
+    Text(text = owner, fontSize = 10.sp)
+}
+
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+fun LazyCol(navController: NavHostController){
+    val context = LocalContext.current
+    val postsLists = getUserInfo(context)
+
+    LazyColumn(content = {
+        stickyHeader {
+            Button(
+                onClick = {navController.navigate("home") },
+                modifier = Modifier.padding(40.dp)
+            ) {
+                Text(text = "Home")
+            }
+        }
+        itemsIndexed(postsLists, itemContent = { index, item ->
+            item.owner?.let { UserPost(it) }
+            val currentImage = item.image?.let { convertImageByteArrayToBitmap(it) }
+            currentImage?.let { BitmapImage(it) }
+        })
+    })
 }
